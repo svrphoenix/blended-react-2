1a84a6d63c6a4915e3d92491acc4fd6c012f3644
Serhii Rudenko <svr.phoenix@gmail.com>
2023-05-07 11:29:27 +0300

    Added fetch images

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,37 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+  handleInput = evt => {
+    this.setState({ query: evt.currentTarget.value.toLowerCase().trim() });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const { query } = this.state;
+    this.props.onSubmit(query);
+    this.setState({ query: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    const { query } = this.state;
+
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          value={query}
+          required
+          autoFocus
+          onChange={this.handleInput}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,40 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    isLoading: false,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getPhotos(query, page);
+    }
+  }
+
+  getPhotos = async (query, page) => {
+    if (!query) return;
+    this.setState({ isLoading: true });
+    try {
+      const data = await ImageService.getImages(query, page);
+      console.log(data);
+    } catch {
+    } finally {
+    }
+  };
+
+  onHandleSubmit = value => {
+    this.setState({ query: value });
+  };
+
   render() {
     return (
       <>
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.onHandleSubmit} />
       </>
     );
   }
